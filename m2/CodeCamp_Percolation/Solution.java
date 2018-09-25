2c2166c238f0ef1fe64a9b932d775ac20a616332
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-25 18:01:25 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- m2/CodeCamp_Percolation/Solution.java
+++ m2/CodeCamp_Percolation/Solution.java
@@ -1,4 +1,4 @@
-// 
+//
 // import java.util.Scanner;
 // class Percolation {
 //     private int[][] id;
@@ -24,7 +24,7 @@
 //         return true;
 //     }
 //     return false;
-//    }  
+//    }
 //    public boolean isFull(int row, int col) {
 //     return false;
 
@@ -64,60 +64,98 @@
 //     }
 // }
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-    public static void main(String[] args) {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = Integer.parseInt(sc.nextLine());
         percolate obj = new percolate(input);
         while(sc.hasNextLine()){
             String[] inputs = sc.nextLine().split(" ");
-            obj.open(Integer.parseInt(inputs[0])-1, Integer.parseInt(inputs[1])-1);
+            obj.open(Integer.parseInt(inputs[0]) - 1, Integer.parseInt(inputs[1]) - 1);
         }
         System.out.println(obj.ispercolate());
     }
 
 }
-
-class percolate{
-    boolean[][] grid;
-    WeightedQuickUnionUF obj;
-    int size;
-    percolate(int size){
+/**
+ * Class for percolate.
+ */
+class percolate {
+    private boolean[][] grid;
+    private WeightedQuickUnionUF obj;
+    private int size;
+    /**
+     * Constructs the object.
+     *
+     * @param      size  The size
+     */
+    percolate(final int size){
         this.size = size;
         grid = new boolean[size][size];
         obj = new WeightedQuickUnionUF(size * size + 2);
     }
-    public void open(int row, int column){
-        if(grid[row][column]){
+    /**
+     * function_description.
+     *
+     * @param      row     The row
+     * @param      column  The column
+     */
+    public void open(final int row, final int column) {
+        if (grid[row][column]) {
             return;
         }
         grid[row][column] = true;
-        if(row == 0){
-            obj.union(convert(row,column), size*size);
+        if (row == 0) {
+            obj.union(convert(row, column), size * size);
         }
-        if(row == size-1){
-            obj.union(convert(row, column), size*size+1);
+        if (row == size - 1) {
+            obj.union(convert(row, column), size * size + 1);
         }
-        if(row < size - 1 && grid[row+1][column]){ //bottom element
-            obj.union(convert(row,column), convert(row+1, column));
+        if (row < size - 1 && grid[row + 1][column]) { //bottom element
+            obj.union(convert(row, column), convert(row + 1, column));
         }
-        if(row > 0 && grid[row-1][column]){ //top element
-            obj.union(convert(row,column), convert(row-1, column));
+        if (row > 0 && grid[row - 1][column]) { //top element
+            obj.union(convert(row, column), convert(row - 1, column));
         }
-        if(column > 0 && grid[row][column-1]){ //left element
-            obj.union(convert(row,column), convert(row, column-1));
+        if (column > 0 && grid[row][column - 1]) { //left element
+            obj.union(convert(row, column), convert(row, column - 1));
         }
-        if(column < size - 1 && grid[row][column+1]){ //right element
-            obj.union(convert(row,column), convert(row, column+1));
+        if (column < size - 1 && grid[row][column + 1]) { //right element
+            obj.union(convert(row, column), convert(row, column + 1));
         }
 
 
     }
-    public boolean ispercolate(){
-        return obj.connected(size * size, size*size+1);
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean ispercolate() {
+        return obj.connected(size * size, size * size + 1);
     }
 
-    public int convert(int i, int j){
+    /**
+     * function_description.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int convert(final int i, final int j) {
         return i * size + j;
     }
 }

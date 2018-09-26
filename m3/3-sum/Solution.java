b6cdcc248c4bca88bd9ccca54aac2dcef8982400
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-26 20:00:50 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -16,27 +16,16 @@ class sum {
     sum() { }
     public void counntof(int[] arr1) {
         int count = 0;
-
-        Arrays.sort(arr1); 
+        Arrays.sort(arr1);
         int size = arr1.length;
         for (int i = 0; i < size; i++) {
             int num = 0;
             for (int j = i+1; j < size; j++) {
                 num = -1 * (arr1[i] + arr1[j]);
-                for (int k = j; k < size;k = k / 2) {
+                for (int k = j+1; k < size;k = k / 2) {
                     if (binarySearch(arr1, num, k)!= -1) {
                         count++;
                     }
-                //     int lo = arr1[k];
-                //     int hi = arr1[size-1];
-                //     int mid = lo + (hi - lo) / 2;
-                //     if (num < arr1[mid]) {
-                //         hi = mid - 1;
-                //     } else if (num > arr1[mid]) {
-                //         lo = mid + 1;
-                //     } else {
-                //         count++;
-                //  }
                 }
             }
         }

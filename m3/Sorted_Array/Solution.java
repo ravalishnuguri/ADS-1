f434d9ceb373f82572d75251367a4c38200897de
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-26 19:33:43 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m3/Sorted_Array/Solution.java
+++ m3/Sorted_Array/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
+final class Solution {
     /**
      * Constructs the object.
      */
@@ -32,13 +32,13 @@ class Solution {
         }
         if (size1 == 0) {
             int h;
-            for(h = 0; h < size2 - 1; h++) {
+            for (h = 0; h < size2 - 1; h++) {
                 System.out.print(secondarr[h] + ",");
             }
             System.out.print(secondarr[h]);
 
         } else {
-        s.sortarray(firstarr,secondarr,size1,size2);
+        s.sortarray(firstarr, secondarr, size1, size2);
         }
     }
 }
@@ -46,15 +46,25 @@ class Solution {
  * Class for sort.
  */
 class Sort {
-    public void sortarray(final int[] a1, final int[] a2, final int len1, final int len2) {
+    /**
+     * function_description.
+     *
+     * @param      a1    A 1
+     * @param      a2    A 2
+     * @param      len1  The length 1
+     * @param      len2  The length 2
+     */
+    public void sortarray(final int[] a1, final int[] a2,
+        final int len1, final int len2) {
         int[] res = new int[len1 + len2];
         int i = 0, j = 0, k = 0;
         int p;
         while (i < len1 && j < len2) { 
-            if (a1[i] < a2[j])
+            if (a1[i] < a2[j]) {
                 res[k++] = a1[i++];
-            else
-                res[k++] = a2[j++]; 
+            } else {
+                res[k++] = a2[j++];
+            }
         }
         while (i < len1) {
             res[k++] = a1[i++];
@@ -63,13 +73,13 @@ class Sort {
             res[k++] = a2[j++];
         }
         while (len1 == 0) {
-            for(p = 0; p < len1; p++) {
+            for (p = 0; p < len1; p++) {
             System.out.print(a1[p] + ",");
         }
         System.out.print(a1[p]);
 
         }
-        for (p = 0; p < k -1; p++) {
+        for (p = 0; p < k - 1; p++) {
             System.out.print(res[p] + ",");
         }
         System.out.print(res[p]);

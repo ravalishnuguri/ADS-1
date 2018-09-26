d2caa964c2bb6d91c8c7de6557f68ec4bc458e4d
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-26 18:52:56 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- m3/Sorted_Array/Solution.java
+++ m3/Sorted_Array/Solution.java
@@ -5,20 +5,12 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         int size1 = sc.nextInt();
         int size2 = sc.nextInt();
-        System.out.println(size1);
-        System.out.println(size2);
-        int[] firstarr = new int[size1];
-        int[] secondarr = new int[size2];
+        sc.nextLine();
+        // int[] firstarr = new int[size1];
+        // int[] secondarr = new int[size2];
         String[] arr1 = sc.nextLine().split(",");
         String[] arr2 = sc.nextLine().split(",");
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));
-        // for (int i = 0;i<size;i++) {
-        //     arr[i] = Integer.parseInt(sc.nextLine());
-        // }
-        // int size = Integer.parseInt(sc.nextLine());
-        // int[] arr = new int[size];
-        // for (int i = 0;i<size;i++) {
-        //     arr[i] = Integer.parseInt(sc.nextLine());
     }
 }

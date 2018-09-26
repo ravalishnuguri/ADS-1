f4a24efd2608ea5f38f6cb62a5093da83c666495
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-26 20:20:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -1,29 +1,52 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
-    public static void main(String[] args) {
+/**
+ * Solution class.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         sum u = new sum();
         int size = Integer.parseInt(sc.nextLine());
         int[] arr = new int[size];
-        for (int i = 0;i<size;i++) {
+        for (int i = 0; i < size; i++) {
             arr[i] = Integer.parseInt(sc.nextLine());
         }
         u.counntof(arr);
     }
 }
+/**
+ * Class for sum.
+ */
 class sum {
+    /**
+     * Constructs the object.
+     */
     sum() { }
-    public void counntof(int[] arr1) {
+    /**
+     * function_description.
+     *
+     * @param      arr1  The arr 1
+     */
+    public void counntof(final int[] arr1) {
         int count = 0;
         Arrays.sort(arr1);
         int size = arr1.length;
-        for (int i = 0; i < size-1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             int num = 0;
-            for (int j = i+1; j < size; j++) {
+            for (int j = i + 1; j < size; j++) {
                 num = -1 * (arr1[i] + arr1[j]);
                 
-                    if (binarySearch(arr1, num, j+1)!= -1) {
+                    if (binarySearch(arr1, num, j + 1) != - 1) {
                         count++;
                     }
                 
@@ -31,8 +54,17 @@ class sum {
         }
         System.out.println(count);
     }
-public static int binarySearch(int[] a, int key, int lo) {
-         int hi = a.length-1;
+    /**
+     * function_description.
+     *
+     * @param      a     { parameter_description }
+     * @param      key   The key
+     * @param      lo    The lower
+     *
+     * @return     { description_of_the_return_value }
+     */
+public static int binarySearch(final int[] a, final int key, int lo) {
+         int hi = a.length - 1;
          while (lo <= hi) {
          int mid = lo + (hi - lo) / 2;
          if (key < a[mid]) {

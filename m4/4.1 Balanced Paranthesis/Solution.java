9542388105465c476e077fc5ce9892e39d027101
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-27 17:16:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/4.1 Balanced Paranthesis/Solution.java
+++ m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,21 +1,57 @@
-import java.util.Scanner;;
+import java.util.Scanner;
+/**
+ * Class for balancedornot.
+ */
 class Balancedornot {
+  /**
+   * List of .
+   */
   private class Stack {
-  int top = -1;
-  char item[] = new char[1000];
-  public void push(char x) {
+    /**
+     * var_description.
+     */
+  private int top = -1;
+  /**
+   * var_description.
+   */
+  final int number = 1000;
+  private char item[] = new char[number];
+  /**
+   * function_description.
+   *
+   * @param      x     { parameter_description }
+   */
+  public void push(final char x) {
   item[++top] = x;
 }
+/**
+ * function_description.
+ *
+ * @return     { description_of_the_return_value }
+ */
   public char pop() {
     char e = item[top];
     top--;
     return e;
     }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
   public boolean isEmpty() {
     return top == -1;
   }
 }
-  public boolean balancedparenthesis(char c1, char c2) {
+/**
+ * function_description.
+ *
+ * @param      c1    The c 1
+ * @param      c2    The c 2
+ *
+ * @return     { description_of_the_return_value }
+ */
+  public boolean balancedparenthesis(final char c1, final char c2) {
        if (c1 == '(' && c2 == ')')
          return true;
        else if (c1 == '{' && c2 == '}')
@@ -25,9 +61,16 @@ class Balancedornot {
        else
          return false;
     }
-  public String balanced(char[] exp) {
+    /**
+     * function_description.
+     *
+     * @param      exp   The exponent
+     *
+     * @return     { description_of_the_return_value }
+     */
+  public String balanced(final char[] exp) {
     Stack st = new Stack();
-    for(int i=0;i<exp.length;i++) {
+    for(int i = 0; i < exp.length; i++) {
       if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[') {
             st.push(exp[i]);
           }
@@ -35,7 +78,7 @@ class Balancedornot {
              if (st.isEmpty()) {
                    return "NO";
                }
-      else if ( !balancedparenthesis(st.pop(), exp[i]) ) {
+      else if (!balancedparenthesis(st.pop(), exp[i])) {
                    return "NO";
                }
           }
@@ -44,15 +87,25 @@ class Balancedornot {
        
       if (st.isEmpty()) {
          return "YES";
-  }
-      else {
+  } else {
              return "NO";
          } 
     }
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-  public static void main(String[] args) {
+  /**
+   * Constructs the object.
+   */
+  private Solution() { }
+  /**
+   * .function_description.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
   Balancedornot b = new Balancedornot();
   Scanner sc = new Scanner(System.in);
   String numberoflines = sc.nextLine();
@@ -63,7 +116,6 @@ public class Solution {
     char[] line = tokens.toCharArray();
     System.out.println(b.balanced(line));
     num++;
+    }
   }
-
-  }
- }
+}

c95a300c5777dd43533b03b629b51cab8aba5ce7
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-28 21:02:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- m5/5.2 Josephus Problem/Solution.java
+++ m5/5.2 Josephus Problem/Solution.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
-import java.util.Arrays;
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		scan.nextLine();
-		for (int i = 0; i < num; i++) {
-			Operations op = new Operations();
-			String jose = scan.nextLine();
-			String[] jos = jose.split(" ");
-			int k = Integer.parseInt(jos[0]);
-			int m = Integer.parseInt(jos[1]);
-			for (int j = 0; j < k; j++) {
-				op.insertAtTail(j);
-			}
-			int[] josephus = op.josephus(m, k);
-			for (int p = 0; p < josephus.length - 1; p++) {
-				System.out.print(josephus[p] + " ");
-			}
-			System.out.println(josephus[josephus.length - 1]);
-		}
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < num; i++) {
+            Operations op = new Operations();
+            String jose = scan.nextLine();
+            String[] jos = jose.split(" ");
+            int k = Integer.parseInt(jos[0]);
+            int m = Integer.parseInt(jos[1]);
+            for (int j = 0; j < k; j++) {
+                op.insertAtTail(j);
+            }
+            int[] josephus = op.josephus(m, k);
+            for (int p = 0; p < josephus.length - 1; p++) {
+                System.out.print(josephus[p] + " ");
+            }
+            System.out.println(josephus[josephus.length - 1]);
+        }
+    }
 }

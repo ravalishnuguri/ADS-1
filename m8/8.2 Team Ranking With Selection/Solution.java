dd48d01fc4f495c1f99cc91dcf4f3e8005c6ea9d
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-02 19:51:49 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- m8/8.2 Team Ranking With Selection/Solution.java
+++ m8/8.2 Team Ranking With Selection/Solution.java
@@ -1,10 +1,27 @@
 import java.util.Scanner;
+/**
+ * item_description.
+ */
 public final class Solution {
+    /**
+     * var_description.
+     */
     private static final int TEN = 10;
+    /**
+     * var_description.
+     */
     private static final int THREE = 3;
+    /**
+     * Constructs the object.
+     */
     private Solution() {
 
     }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Team[] teams = new Team[TEN];
         int size = 0;

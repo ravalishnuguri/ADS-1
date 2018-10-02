2e9bb5a7cc2af66b41bdf3cef786c02dbdbac65e
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-02 19:50:10 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m8/8.2 Team Ranking With Selection/Solution.java
+++ m8/8.2 Team Ranking With Selection/Solution.java
@@ -6,13 +6,13 @@ public final class Solution {
 
     }
     public static void main(final String[] args) {
-        Teamdetails[] teams = new Teamdetails[TEN];
+        Team[] teams = new Team[TEN];
         int size = 0;
         Scanner scan = new Scanner(System.in);
         Selection t = new Selection();
         while (scan.hasNext()) {
             String[] line = scan.nextLine().split(",");
-            teams[size++] = new Teamdetails(line[0], Integer.parseInt(line[1]),
+            teams[size++] = new Team(line[0], Integer.parseInt(line[1]),
                      Integer.parseInt(line[2]), Integer.parseInt(line[THREE]));
         }
         System.out.println(t.leaderboard(teams, size));

c68dc797fb4a532faac633c82757489c3768a727
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-02 19:35:05 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- m8/8.1 Team Ranking With Insertion/Solution.java
+++ m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() { }
-	/**
-	 * function_description.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		Insertion in = new Insertion();
-		while (s.hasNext()) {
-			String line = s.nextLine();
-        	String[] tokens = line.split(",");
-        	in.add(new Teamdetails(tokens[0], Integer.parseInt(tokens[1]),
-        		Integer.parseInt(tokens[2]), Integer.parseInt(tokens[2 + 1])));
-		}
-		in.insertionsort();
-		System.out.println(in);
-	}
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        Insertion in = new Insertion();
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            String[] tokens = line.split(",");
+            in.add(new Teamdetails(tokens[0], Integer.parseInt(tokens[1]),
+        Integer.parseInt(tokens[2]), Integer.parseInt(tokens[2 + 1])));
+        }
+        in.insertionsort();
+        System.out.println(in);
+    }
 }

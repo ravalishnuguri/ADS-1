b7d552526e8ec428f2bd176266845d45eb0a859d
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-02 19:34:03 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m8/8.1 Team Ranking With Insertion/Solution.java
+++ m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,29 +1,28 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() { }
-	public static void main(String[] args) {
+	/**
+	 * function_description.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		Insertion in = new Insertion();
 		while (s.hasNext()) {
 			String line = s.nextLine();
         	String[] tokens = line.split(",");
-        	in.add(new Teamdetails(tokens[0],Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]),Integer.parseInt(tokens[3])));
+        	in.add(new Teamdetails(tokens[0], Integer.parseInt(tokens[1]),
+        		Integer.parseInt(tokens[2]), Integer.parseInt(tokens[2 + 1])));
 		}
 		in.insertionsort();
 		System.out.println(in);
 	}
 }
-
-
- // public static void main(final String[] args) {
- //        Scanner scan = new Scanner(System.in);
- //        InsertionSort isobj = new InsertionSort();
- //        while (scan.hasNext()) {
- //            String[] tokens = scan.nextLine().split(",");
- //            isobj.add(new Tournament(tokens[0], Integer.parseInt(tokens[1]),
- //            Integer.parseInt(tokens[2]), Integer.parseInt(tokens[2 + 1])));
- //        }
- //        isobj.insertionsort();
- //        System.out.println(isobj);
- //    }

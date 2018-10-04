39a6c6c96fd39b7f57868a239eeb6f808fb7c5dd
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-04 20:39:23 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m10/10.1 LinkedList using Recursion/Solution.java
+++ m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,42 +1,56 @@
-import java.util.*;
-
-class Solution{
-    public static void main(String[] args) {
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution{
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() { }
+	/**
+	 * function_description.
+	 *
+	 * @param      args  The arguments
+	 */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         LinkedList ll = new LinkedList();
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             String[] values = sc.nextLine().split(" ");
             switch (values[0]) {
                 case "insertAt":
-                try{
+                try {
                     ll.insertAt(Integer.parseInt(values[1]), new Node(values[2]));
                 ll.display();
                 }
-
                 catch (Exception e) {
                     System.out.println("Can't insert at this position.");
 
                 }
                 break;
                 case "reverse":
-                try{
+                try {
                     ll.reverse();
                 ll.display();
                 }
 
-                catch(Exception e){
+                catch (Exception e) {
                     System.out.println("No elements to reverse.");
                 }
                 break;
-
+                default:
+                break;
             }
         }
     }
 }
+/**
+ * Class for node.
+ */
 class Node {
     String data;
     Node next;
-    Node(String val){
+    Node (String val) {
         data = val;
     }
 }

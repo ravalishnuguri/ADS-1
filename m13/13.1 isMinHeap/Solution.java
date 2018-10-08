5e0ce736024209158877e4681d47a68602c1d016
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-08 19:46:50 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m13/13.1 isMinHeap/Solution.java
+++ m13/13.1 isMinHeap/Solution.java
@@ -2,73 +2,73 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() { }
-	/**
-	 * function_description.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		String s1 = s.nextLine();
-		int n = Integer.parseInt(s.nextLine());
-		switch(s1) {
-			case "String":
-			while (n > 0) {
-			String[] tokens = s.nextLine().split(",");
-			Priority<String> m = new Priority<String>(tokens);
-			System.out.println(m.isminHeap());
-			n--;
-				}
-				break;
-			case "Integer":
-			while (n > 0) {
-			String[] tokens = s.nextLine().split(",");
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        int n = Integer.parseInt(s.nextLine());
+        switch (s1) {
+            case "String":
+            while (n > 0) {
+            String[] tokens = s.nextLine().split(",");
+            Priority<String> m = new Priority<String>(tokens);
+            System.out.println(m.isminHeap());
+            n--;
+                }
+                break;
+            case "Integer":
+            while (n > 0) {
+            String[] tokens = s.nextLine().split(",");
             Integer[] intTokens = new Integer[tokens.length];
-            for (int i = 0; i < tokens.length;i++) {
-               	intTokens[i] = Integer.parseInt(tokens[i]);
+            for (int i = 0; i < tokens.length; i++) {
+                intTokens[i] = Integer.parseInt(tokens[i]);
                }
             Priority<Integer> m = new Priority<Integer>(intTokens);
             System.out.println(m.isminHeap());
             n--;
-				}
-				break;
-			case "Double":
-			while (n > 0) {
-			String[] tokens = s.nextLine().split(",");
+                }
+                break;
+            case "Double":
+            while (n > 0) {
+            String[] tokens = s.nextLine().split(",");
             Double[] idTokens = new Double[tokens.length];
-            for (int i = 0; i < tokens.length;i++) {
-               	idTokens[i] = Double.parseDouble(tokens[i]);
-           	}
+            for (int i = 0; i < tokens.length; i++) {
+                idTokens[i] = Double.parseDouble(tokens[i]);
+            }
             Priority<Double> m = new Priority<Double>(idTokens);
             System.out.println(m.isminHeap());
             n--;
-				}
-		break;
-		case "Float":
-		while (n > 0) {
-		String[] tokens = s.nextLine().split(",");
-		if(tokens[0].equals("")) {
-			System.out.println("false");
-			break;
-		} else {
-		Float[] ftokens = new Float[tokens.length];
-		for (int i = 0; i < tokens.length; i++) {
-				ftokens[i] = Float.parseFloat(tokens[i]);
-			}
-		Priority<Float> m = new Priority<Float>(ftokens);
-		System.out.println(m.isminHeap());
-		n--;
-			}
-		}
-		break;
-		default:
-		break;
-	}
+                }
+        break;
+        case "Float":
+        while (n > 0) {
+        String[] tokens = s.nextLine().split(",");
+        if (tokens[0].equals("")) {
+            System.out.println("false");
+            break;
+        } else {
+        Float[] ftokens = new Float[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+                ftokens[i] = Float.parseFloat(tokens[i]);
+            }
+        Priority<Float> m = new Priority<Float>(ftokens);
+        System.out.println(m.isminHeap());
+        n--;
+            }
+        }
+        break;
+        default:
+        break;
+    }
 
-	}
+    }
 }

1f51c2028f0e6dd9d754a0a911246144f57baba6
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-22 11:34:07 +0530

    "ads1_m19_a1: 0/0 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ m19/19.1 BST/BST.java
@@ -0,0 +1,101 @@
+class BST<Key extends Comparable<Key>, Value> {
+    Node root;
+    int size = 0;
+    private class Node {
+        private Book key;
+        private Value value;
+        private Node left;
+        private Node right;
+    }
+    public void put(Book key, Value value) {
+        if (key == null)  {
+            System.out.println("null");
+        }
+        root = put(root, key, value);
+    }
+    /**
+     * the time complexity for the method is O(logN).
+     *
+     * @param      root   The root
+     * @param      key    The key
+     * @param      value  The value
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node put(Node root, Book key, Value value) {
+        if (root == null) {
+            Node n = new Node();
+            n.key = key;
+            n.value = value;
+            n.left = null;
+            n.right = null;
+            root = n;
+            size++;
+        }
+        int cmp = key.compareTo(root.key);
+        if(cmp < 0) {
+            root.left  = put(root.left,  key, value);
+        }
+        else if (cmp > 0) {
+            root.right = put(root.right, key, value);
+        }
+        else {
+            root.value = value;
+        }
+        return root;
+
+    }
+    public Value get(Book key) {
+        return get(root, key);
+    }
+    /**
+     * the time complexity for the method is O(logN).
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Value get(Node x, Book key) {
+        if (key == null) {
+            System.out.println("empty");
+        }
+        if (x == null) {
+            //System.out.println("in get");
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        }
+        else if (cmp > 0) {
+            return get(x.right, key);
+        }
+        else {
+            return x.value;
+        }
+    }
+
+    public void min() {
+        System.out.println("still coding");
+
+    }
+    public void max() {
+        System.out.println("still coding");
+    }
+    public Book floor(Book key) {
+        return floor(root, key);
+    }
+    private Book floor(Node x, Book key) {
+        return key;
+    }
+    public Book ceiling(Book key) {
+        return ceiling(root, key);
+    }
+    private Book ceiling(Node x, Book key) {
+        return key;
+    }
+    public void select(int index) {
+    System.out.println("still coding");   
+    }
+}

1df6c845e372550534c5654248114f7b433232e1
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-22 12:02:34 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ a/Solution.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * { function_description }.
+     * the time complexity for the main method is O(n).
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BST<String, Integer> bs = new BST<String, Integer>();
+        while (sc.hasNextLine()) {
+            String st = sc.nextLine();
+            String[] tokens = st.split(",");
+            Book b = new Book(tokens[1], tokens[2],
+                Float.parseFloat(tokens[2 + 1]));
+            switch (tokens[0]) {
+                case "put":
+                bs.put(b, Integer.parseInt(tokens[2 + 2]));
+                break;
+                case "get":
+                System.out.println(bs.get(b));
+                break;
+                case "max":
+                bs.max();
+                break;
+                case "min":
+                bs.min();
+                break;
+                case "floor":
+                System.out.println(bs.floor(b));
+                break;
+                case "ceiling":
+                System.out.println(bs.ceiling(b));
+                break;
+                case "select":
+                bs.select(Integer.parseInt(tokens[1]));
+                break;
+                default:
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * Class for book.
+ */
+class Book {
+    /**
+     * { var_description.
+     */
+    private String name;
+    /**
+     * { var_description.
+     */
+    private String author;
+    /**
+     * var_description.
+     */
+    private Float price;
+    /**
+     * Constructs the object.
+     */
+    private Book() { }
+    /**
+     * Constructs the object.
+     *
+     * @param      bname    The bname
+     * @param      bauthor  The bauthor
+     * @param      bprice   The bprice
+     */
+    Book(final String bname, final String bauthor,
+        final Float bprice) {
+        this.name = bname;
+        this.author = bauthor;
+        this.price = bprice;
+    }
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+     public String getname() {
+        return this.name;
+    }
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String getauthor() {
+        return this.author;
+    }
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Float getprice() {
+        return this.price;
+    }
+    /**
+     * function_description.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Book that) {
+        if (this.getname().compareTo(that.getname()) > 0) {
+            return 1;
+        } else if (this.getname().compareTo(that.getname()) < 0) {
+            return -1;
+        } else if (this.getauthor().compareTo(that.getauthor()) < 0) {
+            return -1;
+        } else if (this.getauthor().compareTo(that.getauthor()) > 0) {
+            return 1;
+        } else if (this.getprice() > that.getprice()) {
+            return 1;
+        } else if (this.getprice() < that.getprice()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}

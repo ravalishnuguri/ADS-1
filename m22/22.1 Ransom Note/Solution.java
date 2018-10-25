dd626d69375968400e42ce92fa7565f23736d4b8
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-25 21:22:34 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ m22/22.1 Ransom Note/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.HashMap;
+class Solution {
+private Solution() {
+
+}
+public static void main(final String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int m = scan.nextInt();
+	int n = scan.nextInt();
+	scan.nextLine();
+	String[] magazine = scan.nextLine().split(" ");
+	String[] note = scan.nextLine().split(" ");
+	HashMap<String, Integer> hm = new HashMap<String, Integer>();
+	for(int i = 0; i < m; i++) {
+		if(hm.containsKey(magazine[i])) {
+			hm.put(magazine[i], hm.get(magazine[i]) + 1);
+		}else{
+			hm.put(magazine[i], 1);
+		}
+	}
+	boolean check = false;
+	for (int i = 0; i < n; i++) {
+		if (!hm.containsKey(note[i])) {
+			check = true;
+		} else {
+			if (hm.get(note[i]) > 1) {
+				hm.put(note[i], hm.get(note[i]) - 1);
+			} else {
+				hm.remove(note[i]);
+			}
+		}
+	}
+	if (!check) {
+		System.out.println("Yes");
+	} else {
+	System.out.println("No");
+		}
+	}
+}

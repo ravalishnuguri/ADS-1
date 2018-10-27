5a4ccd5fef3123d3311bc47e906499eee4e6cd15
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-10-27 13:07:57 +0530

    "m24_a1_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m24/1/Solution.java
+++ m24/1/Solution.java
@@ -30,7 +30,7 @@ final class Solution {
  */
 public static void put1(Student[] a, int x) {
     for (int i = 0; i < a.length; i++) {
-            if (a[i].getRollno() == x) {
+            if (a[i].getRollno()== x) {
                 System.out.println(a[i].getMarks());
             }
         }
